28b344a14f5e989c9dc2461457557d0df25d8407
Sander Brustad <sander.simonsen.brustad@gmail.com>
2023-02-01 13:19:59 +0100

    Add calculator functions and design change

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,29 @@
 "use strict"
 
+
+
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+
+function subtract(num1, num2) {
+    let sum = num1 - num2;
+    return sum;
+
+};
+
+function multiply(num1, num2) {
+    let sum = num1 * num2;
+    return sum;
+};
+
+function divide(num1, num2) {
+    let sum = num1 / num2;
+    return sum;
+};
+
+function operate(operator, num1, num2) {
+    let sum = operator(num1, num2);
+    return sum;
+};
