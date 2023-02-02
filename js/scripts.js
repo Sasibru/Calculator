cb45744eae5a180f21937c036b913373c736c180
Sander Brustad <sander.simonsen.brustad@gmail.com>
2023-02-02 13:24:00 +0100

    Add fix to operate function and wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,49 @@
 "use strict"
 
-const numberButtons = document.querySelectorAll("[data-number]");
+const displayNumber = document.getElementById("displayNumbers");
+const numberButtons = document.querySelectorAll("numbers");
 const operatorButtons = document.querySelectorAll("[data-operator]");
+let displayValue = "";
 
+numberButtons.forEach(button => {
+    button.addEventListener("click", event =>{
+      return displayValue.textContent = event.value;
+    })
+})
 
 
 function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+    return num1 + num2;
 };
 
 function subtract(num1, num2) {
-    let sum = num1 - num2;
-    return sum;
+    return num1 - num2;
 
 };
 
 function multiply(num1, num2) {
-    let sum = num1 * num2;
-    return sum;
+    return num1 * num2;
 };
 
 function divide(num1, num2) {
-    let sum = num1 / num2;
-    return sum;
+    return num1 / num2;
 };
 
 function operate(operator, num1, num2) {
-    let sum = operator(num1, num2);
-    return sum;
+    switch(operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            return null;
+    };
 };
